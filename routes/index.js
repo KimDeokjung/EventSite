332db254c52874cbdf9353fdbfc32d8b30bd16bf
KimDeokjung <ejrwnd123@gmail.com>
2023-10-17 23:50:47 +0900

    반응속도 게임

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-
   res.cookie('rememberme', '1', { expires: new Date(Date.now() + 36000000) })
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/light', function(req, res, next) {
+  res.render('light', { title: 'Express' });
+});
 
 
 module.exports = router;
